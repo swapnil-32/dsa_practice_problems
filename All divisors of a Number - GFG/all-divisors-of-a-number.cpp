48a4c872d5d098e00c203291289db8d3fa70bcf1
swapnil-32 <114354201+swapnil-32@users.noreply.github.com>
2023-09-04 00:16:22 +0530

    Added solution - LeetHub

---
+++ All divisors of a Number - GFG/all-divisors-of-a-number.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void print_divisors(int n) {
+        vector<int>ans;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                ans.push_back(i);
+                if(i!=n/i){
+                    ans.push_back(n/i);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        // Code here.
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        ob.print_divisors(n);
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
