e2cf04104720f3f1a2fb8a305db186ac26c5c0a7
swapnil-32 <114354201+swapnil-32@users.noreply.github.com>
2023-09-04 14:37:04 +0530

    Added solution - LeetHub

---
+++ Celsius to Fahrenheit Conversion - GFG/celsius-to-fahrenheit-conversion.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    double celciusToFahrenheit(int C){
+        double ans=1.8*C;
+        // double a=ans*C;
+        return 32+ans;
+        //code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout <<setprecision(2)<<fixed<< ob.celciusToFahrenheit(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
