0c3855e1c1e3e026bee9c68a3c94d08653dceb47
swapnil-32 <114354201+swapnil-32@users.noreply.github.com>
2023-09-06 23:50:26 +0530

    Added solution - LeetHub

---
+++ Tower Of Hanoi - GFG/tower-of-hanoi.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // You need to complete this function
+
+    // avoid space at the starting of the string in "move disk....."
+    long long toh(int N, int from, int to, int aux) {
+        int c=1;
+        if(N==1){
+            cout<<"move disk 1 from rod "<<from<<" to rod "<<to<<endl;
+            return N;
+        }
+        c+=toh(N-1,from,aux,to);
+        cout<<"move disk "<<N<<" from rod "<<from<<" to rod "<<to<<endl;
+        c+=toh(N-1,aux,to,from);
+        return c;
+        // Your code here
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;//testcases
+    while (T--) {
+        
+        int N;
+        cin >> N;//taking input N
+        
+        //calling toh() function
+        Solution ob;
+        
+        cout << ob.toh(N, 1, 3, 2) << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
