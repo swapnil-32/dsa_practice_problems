f4e0fc2cca621b790313d475cbf650b556fdac5c
swapnil-32 <114354201+swapnil-32@users.noreply.github.com>
2023-09-07 19:18:39 +0530

    Added solution - LeetHub

---
+++ Josephus problem - GFG/josephus-problem.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution
+{
+    public:
+    int jos(int n,int k){
+        if(n==1){
+            return 0;
+        }
+        int a=(josephus(n-1,k)+k-1)%n;
+        return a;
+        
+    }
+    int josephus(int n, int k)
+    {
+        int ans=jos(n,k);
+        // cout<<ans;
+        return ans+1;
+       //Your code here
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;//taking input n and k
+		
+		//calling josephus() function
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
