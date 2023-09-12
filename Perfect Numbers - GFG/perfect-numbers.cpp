b3319dcfb298ee2176483327700f66056be28d52
swapnil-32 <114354201+swapnil-32@users.noreply.github.com>
2023-09-12 14:38:02 +0530

    Added solution - LeetHub

--- Perfect Numbers - GFG/perfect-numbers.cpp
+++ Perfect Numbers - GFG/perfect-numbers.cpp
@@ -6,32 +6,37 @@ using namespace std;
 class Solution {
   public:
     int isPerfectNumber(long long N) {
-        if(N==8589869056){
-            return 1;
+    //     if(N==8589869056){
+    //         return 1;
+    //     }
+    //     if(N==137438691328){
+    //         return 1;
+    //     }
+    //   if(N==6){
+    //         return true;
+    //     }
+    //     if(N<=10){
+    //         return false;
+    //     }
+    //     int s=0;
+    //     int i;
+    //     for(i=1;i<sqrt(N);i++){
+    //         if(N%i==0){
+    //             s+=i;
+    //         }
+    //     }
+    //     for(;i>1;i--){
+    //         if(N%i==0){
+    //             s+=N/i;
+    //         }
+    //     }
+    //     // cout<<s;
+    //     return s==N;
+    long long sum = 1;
+        for(int i = 2; i<=sqrt(N); i++){
+            if(N % i == 0) sum += (i + N/i);
         }
-        if(N==137438691328){
-            return 1;
-        }
-       if(N==6){
-            return true;
-        }
-        if(N<=10){
-            return false;
-        }
-        int s=0;
-        int i;
-        for(i=1;i<sqrt(N);i++){
-            if(N%i==0){
-                s+=i;
-            }
-        }
-        for(;i>1;i--){
-            if(N%i==0){
-                s+=N/i;
-            }
-        }
-        // cout<<s;
-        return s==N;
+        return sum == N && N != 1;
         // code here
     }
 };
