76dd6a03d6f907aaf9927ee51ff94ba7712fd89c
swapnil-32 <114354201+swapnil-32@users.noreply.github.com>
2023-10-04 18:44:13 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.cpp
+++ Roman Number to Integer - GFG/roman-number-to-integer.cpp
@@ -19,26 +19,30 @@ class Solution {
         m['C']=100;
         m['D']=500;
         m['M']=1000;
-        int ans=m[str[0]];
-        for(int i=1;i<str.length();i++){
-            if((str[i]=='V' || str[i]=='X' || str[i]=='L' || str[i]=='C' || str[i]=='D' || str[i]=='M') && str[i-1]=='I'){
-                ans-=1;
-                ans+=(m[str[i]]-1);
-            }
-             else if((str[i]=='L' ||str[i]=='C' || str[i]=='D' || str[i]=='M' ) && str[i-1]=='X'){
-                ans-=10;
-                ans+=(m[str[i]]-10);
-            }
-            else if((str[i]=='D' || str[i]=='M' ) && str[i-1]=='C'){
-                ans-=100;
-                ans+=(m[str[i]]-100);
-            }
-            //  else if(str[i]=='L' &&nstr[i-1]=='X'){
-            //     ans-=10;
-            //     ans+=(m[str[i]]-10);
-            // }
-            else{
-                ans+=m[str[i]];
+        // int ans=m[str[0]];
+        // for(int i=1;i<str.length();i++){
+        //     if((str[i]=='V' || str[i]=='X' || str[i]=='L' || str[i]=='C' || str[i]=='D' || str[i]=='M') && str[i-1]=='I'){
+        //         ans-=1;
+        //         ans+=(m[str[i]]-1);
+        //     }
+        //      else if((str[i]=='L' ||str[i]=='C' || str[i]=='D' || str[i]=='M' ) && str[i-1]=='X'){
+        //         ans-=10;
+        //         ans+=(m[str[i]]-10);
+        //     }
+        //     else if((str[i]=='D' || str[i]=='M' ) && str[i-1]=='C'){
+        //         ans-=100;                                                           //this is also correct
+        //         ans+=(m[str[i]]-100);
+        //     }
+        //     else{
+        //         ans+=m[str[i]];
+        //     }
+        // }
+        int ans=0;
+        for(int i=0; i<str.size(); i++){
+            if(i < str.length() - 1 && m[str[i]] < m[str[i+1]]){
+                ans -= m[str[i]];
+            }else{
+                ans +=m[str[i]];
             }
         }
         return ans;
